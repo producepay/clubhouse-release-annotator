13edf13e5aad7e40c9e9567d1676edc3755f1d6f
Evan Dorn <evan.dorn@producepay.com>
2018-09-26 13:53:26 -0700

    repository class finds clubhouse ticket numbers in current branch

--- lib/clubhouse-release-annotator.rb
+++ lib/clubhouse-release-annotator.rb
@@ -1,9 +1,14 @@
 require "clubhouse-release-annotator/version"
+require "clubhouse-release-annotator/repository"
 
 module ClubhouseReleaseAnnotator
   class CLI
     def self.run
       puts "release annotator running"
+      repo = Repository.new
+      require 'pp'
+
+      pp repo.tickets
     end
   end
   # Your code goes here
